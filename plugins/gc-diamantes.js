4920d1f8e634c8b60ae4d53339c93f1669bb8f2e
RomiYhair <154240756+RomiYhair@users.noreply.github.com>
2023-12-18 22:09:31 -0500

    🕷️ 𝗦𝗣𝗜𝗗𝗘𝗥 𝗕𝗢𝗧🕷️
    🕷️ 𝗦𝗣𝗜𝗗𝗘𝗥 𝗕𝗢𝗧🕷️

---
+++ plugins/gc-diamantes.js
@@ -0,0 +1,16 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.diamantes);
+};
+handler.command = /^(Diamantes)$/i;
+export default handler;
+
+global.diamante = `
+🕷 SPIDER BOT 🕷
+
+Metodo De Pago : 💎
+
+200 DIAMANTES POR GRUPO 
+
+700 DIAMANTES BOT PROPIO
+
+`;
---
+++ plugins/gc-diamantes1.js
@@ -0,0 +1,25 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.diamantes1);
+};
+handler.command = /^(Diamantes1)$/i;
+export default handler;
+
+global.diamante1 = `
+🕷 SPIDER VENTAS PERU 🕷
+
+VENTAS DE DIAMANTES 💎
+
+110 Diamantes = 4 Soles
+341 Diamantes = 11 Soles
+572 Diamantes = 18 Soles
+1166 Diamantes = 33 Soles
+2398 Diamantes = 66 Soles
+6160 Diamantes = 166 Soles
+ 
+Tarjeta 💎
+
+Semanal : 8 Soles
+Mensual : 37 Soles
+
+Todo Via ID , Yape O Deposito 🕷
+`;
