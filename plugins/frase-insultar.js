a3546b16398180a6e38393d0a2f93adfb1057627
RomiYhair <154240756+RomiYhair@users.noreply.github.com>
2023-12-19 20:49:44 -0500

    Update frase-insultar.js

--- plugins/frase-insultar.js
+++ plugins/frase-insultar.js
@@ -2,14 +2,8 @@ import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {conn, command}) => {
   if (command === 'insulto') {
-    const insulto = insulto[Math.floor(Math.random() * insulto.length)];
-    const mensaje = `🌟 *Consejo del día* \n\n❥ ${insulto}\n\n`;
-    await m.reply(mensaje);
-  }
-
-  if (command === 'fraseromantica') {
     const frase_romantica = frasesromanticas[Math.floor(Math.random() * frasesromanticas.length)];
-    const mensaje = `╭─◆────◈⚘◈─────◆─╮\n\n⠀⠀💖 *Frase romántica* 💖\n\n❥ ${frase_romantica}\n\n╰─◆────◈⚘◈─────◆─╯`;
+    const mensaje = `╭─◆────◈⚘◈─────◆─╮\n\n⠀⠀💖 *insulto* 💖\n\n❥ ${frase_romantica}\n\n╰─◆────◈⚘◈─────◆─╯`;
     await m.reply(mensaje);
   }
 
@@ -31,7 +25,7 @@ ${storytime.text}`, m);
   }
 };
 handler.tags = ['frases'];
-handler.command = handler.help = ['insulto', 'fraseromantica', 'historiaromantica'];
+handler.command = handler.help = ['insulto', 'historiaromantica'];
 export default handler;
 
 async function cerpen(category) {
@@ -62,7 +56,7 @@ async function cerpen(category) {
   });
 }
 
-global.frasesromanticas = [
+global.insulto = [
   'Eres la luz que ilumina mi vida en la oscuridad.',
   'Contigo, cada día es una nueva aventura llena de amor.',
   'Tus ojos son el reflejo del cielo en el que quiero perderme.',
@@ -93,47 +87,4 @@ global.frasesromanticas = [
   'El amor es el idioma en el que nuestros corazones conversan.',
   'Eres el pedacito que me faltaba para completar mi alma.',
   'Amar es encontrar en ti todo lo que nunca supe que necesitaba.',
-];
-
-global.insulto = [
-  'Acepta que los cambios son parte natural de la vida, y aprende a adaptarte a ellos.',
-  'Nunca dejes de aprender; el conocimiento es una herramienta poderosa.',
-  'Cuida de tu salud física y mental, son fundamentales para una vida plena.',
-  'Disfruta de las pequeñas cosas, pues son ellas las que dan sentido a la vida.',
-  'Aprende a perdonar, tanto a los demás como a ti mismo, para liberar tu corazón.',
-  'Valora el tiempo que pasas con tus seres queridos, es el regalo más valioso que puedes dar y recibir.',
-  'Sé amable y compasivo con los demás, cada acto de bondad puede marcar la diferencia en sus vidas.',
-  'Aprende a decir \'no\' cuando sea necesario, y establece límites saludables.',
-  'Encuentra tiempo para hacer lo que te apasiona, pues eso nutre tu alma y te hace sentir vivo.',
-  'No te compares con los demás, cada persona tiene su propio camino y ritmo en la vida.',
-  'Escucha a tu pareja con empatía y comprensión, la comunicación es la base de una relación sólida.',
-  'No tengas miedo de expresar tus sentimientos, la honestidad es esencial en el amor.',
-  'Aprende a ceder y a comprometerte, el amor requiere de sacrificio y esfuerzo mutuo.',
-  'Sorprende a tu pareja de vez en cuando, mantén viva la chispa del romance.',
-  'Respeta la individualidad de tu pareja y permítele crecer como persona.',
-  'El amor propio es igual de importante que amar a alguien más; cuídate y valórate.',
-  'Recuerda que una relación sana se basa en la confianza mutua y el respeto.',
-  'Elige a alguien que te complemente y te haga ser una mejor versión de ti mismo.',
-  'El amor verdadero no te hace sentir menos, te hace sentir más.',
-  'Amar es un verbo, es una elección diaria que se cultiva con acciones y palabras.',
-  'Encuentra un trabajo que te apasione, y nunca sentirás que estás trabajando.',
-  'Sé proactivo y toma la iniciativa en tu trabajo, eso será valorado por tus superiores.',
-  'Aprende de tus errores y fracasos, son oportunidades para crecer y mejorar.',
-  'Mantén una actitud positiva y busca soluciones ante los desafíos laborales.',
-  'Cultiva buenas relaciones con tus colegas, el trabajo en equipo es clave para el éxito.',
-  'Establece metas claras y realistas, y trabaja con determinación para alcanzarlas.',
-  'No tengas miedo de pedir ayuda o buscar mentoría, siempre hay algo nuevo que aprender.',
-  'Reconoce y valora tus logros, celebra tus éxitos por pequeños que sean.',
-  'Busca un equilibrio entre tu vida laboral y personal, ambos aspectos son importantes.',
-  'El trabajo es una parte importante de tu vida, pero no es lo único que define quién eres.',
-  'Cree en ti mismo y en tu capacidad para lograr lo que te propongas.',
-  'Visualiza tus metas y sueños, imagina cómo te sentirás al alcanzarlos.',
-  'Encuentra inspiración en aquellos que han superado obstáculos similares a los tuyos.',
-  'Acepta los fracasos como parte del proceso, son oportunidades para aprender y crecer.',
-  'Rodéate de personas positivas y que te impulsen hacia adelante.',
-  'Mantén una mentalidad abierta y dispuesta a aprender cosas nuevas.',
-  'Recuerda por qué empezaste cuando te sientas desmotivado; reconecta con tu propósito.',
-  'Divide tus metas en pequeños pasos, eso hará el camino más alcanzable y menos abrumador.',
-  'No tengas miedo de perseguir tus sueños, la vida es demasiado corta para vivir con arrepentimientos.',
-  'Confía en que, con esfuerzo y perseverancia, puedes lograr todo lo que te propongas.',
 ];
