0142c369e6abfcfb7bcfaf49e885540cff58fbb9
RomiYhair <154240756+RomiYhair@users.noreply.github.com>
2023-12-21 23:45:43 -0500

    🕷️ 𝗦𝗣𝗜𝗗𝗘𝗥 𝗕𝗢𝗧 🕷️

--- plugins/gc-setname.js
+++ plugins/gc-setname.js
@@ -1,6 +1,6 @@
 import Presence from '@whiskeysockets/baileys';
 const handler = async (m, {conn, args, text}) => {
-  if (!text) throw `𝗜𝗻𝗴𝗿𝗲𝘀𝗮 𝗘𝗹 𝗡𝘂𝗲𝘃𝗼 𝗡𝗼𝗺𝗯𝗿𝗲 𝗗𝗲𝗟 𝗚𝗿𝘂𝗽𝗼 🕷️\n\n𝐄𝐣𝐞𝐦𝐩𝐥𝐨 :\n.𝐬𝐞𝐭𝐧𝐚𝐦𝐞 𝐒𝐩𝐢𝐝𝐞𝐫 𝐁𝐨𝐭 🕷️`;
+  if (!text) throw `𝗜𝗻𝗴𝗿𝗲𝘀𝗮 𝗘𝗹 𝗡𝘂𝗲𝘃𝗼 𝗡𝗼𝗺𝗯𝗿𝗲 𝗗𝗲𝗟 𝗚𝗿𝘂𝗽𝗼 🕷️\n\n𝐄𝐣𝐞𝐦𝐩𝐥𝐨 :\n.𝘀𝗲𝘁𝗻𝗮𝗺𝗲 𝗦𝗽𝗶𝗱𝗲𝗿 𝗕𝗼𝘁 🕷️`;
   try {
     const text = args.join` `;
     if (!args || !args[0]) {
